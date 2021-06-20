ef2622141bf5cc27c30201175ddbf94db6661ac6
lan.yu <fish518518@hotmail.com>
2021-06-19 20:05:24 -0700

    739

---
+++ MonoStack/739. Daily Temperatures.java
@@ -0,0 +1,42 @@
+/**https://leetcode.com/problems/daily-temperatures/
+ * 时间复杂度：o(N)
+ * 空间复杂度：o(1)
+ * Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+ 
+
+Constraints:
+
+1 <= temperatures.length <= 105
+30 <= temperatures[i] <= 100
+
+ */
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] ans = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();//idx
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            int temperature = temperatures[i];
+            while (!stack.isEmpty() && temperatures[stack.peek()] <= temperature) {
+                stack.pop();
+            }
+            ans[i] = stack.isEmpty() ? 0 : (stack.peek() - i);
+            stack.push(i);
+        }
+        return ans;
+    }
+}
